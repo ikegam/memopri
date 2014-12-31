9bf1699478c186369c5941c4504160c3c44a6b3a
Hiroyuki Ikegami <ikegam@mixallow.net>
2015-01-01 00:36:38 +0900

    version 0.0.1

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 This is a command line based application to use Casio's Memopri (MEP-F10).
 
+# Installation
+
+* gem install memopri
+
+# Usage
+
+* echo -e "hoge\nhoge" | memopri
+
 # Test environment
 
 Debian GNU/Linux w/ ruby 2.1.5p273 (2014-11-13) [x86-64-linux-gnu]
--- lib/memopri/renderer.rb
+++ lib/memopri/renderer.rb
@@ -4,71 +4,70 @@ require 'cairo'
 require 'pango'
 
 class Memopri
-class Renderer
+  class Renderer
 
-  attr_accessor :str, :nol
+    attr_accessor :str, :nol
 
-  def initialize(str="", nol=1)
-    @str = str
-    @nol = nol
-  end
+    def initialize(str="")
+      @str = str
+    end
 
-  def conv()
-    format = Cairo::FORMAT_ARGB32
-    width = nil
-    height = 128
-    surface = Cairo::ImageSurface.new(format, 0, 128)
-    context = Cairo::Context.new(surface)
-    layout = context.create_pango_layout
-    layout.text = str
-    font_description = Pango::FontDescription.new("Sans-serif 18")
+    def conv()
+      format = Cairo::FORMAT_ARGB32
+      width = nil
+      height = 128
+      surface = Cairo::ImageSurface.new(format, 0, 128)
+      context = Cairo::Context.new(surface)
+      layout = context.create_pango_layout
+      layout.text = str
+      font_description = Pango::FontDescription.new("Sans-serif 18")
 
-    size = nil
-    begin
-      font_description.size *= 1.06
+      size = nil
+      begin
+        font_description.size *= 1.06
+        layout.font_description = font_description
+        size = layout.size.map{|x| x/Pango::SCALE}
+      end while size[1] <= 111
+
+      width = size[0] + 5
+
+      surface = Cairo::ImageSurface.new(format, width, 128)
+      context = Cairo::Context.new(surface)
+      layout = context.create_pango_layout
+      layout.text = str
+      #layout.width = width * Pango::SCALE * 0.9
+      layout.wrap = Pango::WRAP_CHAR
       layout.font_description = font_description
-      size = layout.size.map{|x| x/Pango::SCALE}
-    end while size[1] <= ((125/@nol.to_f) - 2)
+      context.translate(0, 7)
+      context.show_pango_layout(layout)
+      context.show_page
 
-    width = (size[0])/(@nol.to_f)
+      surface.write_to_png("/tmp/hinomaru.png")
 
-    surface = Cairo::ImageSurface.new(format, width, 128)
-    context = Cairo::Context.new(surface)
-    layout = context.create_pango_layout
-    layout.text = str
-    layout.width = width * Pango::SCALE
-    layout.wrap = Pango::WRAP_CHAR
-    layout.font_description = font_description
-    context.translate(5, 5)
-    context.show_pango_layout(layout)
-    context.show_page
+      data = []
 
-    surface.write_to_png("/tmp/hinomaru.png")
+      surface.data.unpack("C*").each_slice(4) {|x|
+        x = x[3] > 10 ? 1 : 0
+        data.push(x)
+      }
 
-    data = []
+      data2 = []
+      data.each_slice(width) {|x|
+        data2.push(x)
+      }
+      data = data2.transpose.flatten
 
-    surface.data.unpack("C*").each_slice(4) {|x|
-      x = x[3] > 10 ? 1 : 0
-      data.push(x)
-    }
-
-    data2 = []
-    data.each_slice(width) {|x|
-      data2.push(x)
-    }
-    data = data2.transpose.flatten
-
-    ret = []
-    data.each_slice(8) {|x|
-      ret.push([x.join()].pack("B*"))
-    }
-    ret = ret.map{|x| x.unpack("C*")[0]}
-    return ret
+      ret = []
+      data.each_slice(8) {|x|
+        ret.push([x.join()].pack("B*"))
+      }
+      ret = ret.map{|x| x.unpack("C*")[0]}
+      return ret
+    end
   end
-end
 end
 
 if $0 == __FILE__
-  r = Memopri::Renderer.new("あああああ", 3)
+  r = Memopri::Renderer.new("aaa\naa\naaa")
   r.conv()
 end
